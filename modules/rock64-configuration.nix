47fa4189c10157a758fbb309168c983e25922513
Andrew Childs <lorne@cons.org.nz>
2017-12-31 23:29:45 +0900

    Fix networking performance

--- modules/rock64-configuration.nix
+++ modules/rock64-configuration.nix
@@ -13,4 +13,17 @@
   ];
 
   services.mingetty.serialSpeed = [ 1500000 115200 ];
+
+  # Ideally this would be run before the interface is brought up, but
+  # that doesn't seem to be supported by the driver.
+  systemd.services.rock64-fix-ethernet-offload = {
+    description = "Disable offload for rock64 ethernet";
+    after = [ "network-online.target" ];
+    requires = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -K eth0 rx off tx off";
+    };
+  };
 }
