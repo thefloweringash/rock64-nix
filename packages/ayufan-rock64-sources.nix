77deb217f5be59e2208b828dd0a408b037ed8110
Andrew Childs <lorne@cons.org.nz>
2018-01-28 01:52:44 +0900

    0.6.14 -> 0.6.16

--- packages/ayufan-rock64-sources.nix
+++ packages/ayufan-rock64-sources.nix
@@ -1,19 +1,19 @@
 {
-  version = "0.6.14";
+  version = "0.6.16";
   linux-kernel = {
-    rev = "ayufan-rock64/linux-build/0.6.14";
+    rev = "ayufan-rock64/linux-build/0.6.16";
     sha256 = "11kbdq9rr5cx32sbbzcgxfi14mlgavbvmk1glq6vbq992n5v1ab4";
   };
   linux-mainline-kernel = {
-    rev = "ayufan-rock64/linux-build/0.6.14";
+    rev = "ayufan-rock64/linux-build/0.6.16";
     sha256 = "05a1hpfzmggkfsfdjm8la5469fkg1bb7ain5j0gcv6sp05w046cl";
   };
   linux-u-boot = {
-    rev = "ayufan-rock64/linux-build/0.6.14";
-    sha256 = "0gjpsjavf8schpac7b82wq5s58w25a6v27dvdw2jjpkrzsnacr8d";
+    rev = "ayufan-rock64/linux-build/0.6.16";
+    sha256 = "1024pamidiwaxs9qkqdbn2lv0b2ha5qh6izs8h58d1wqsis9pn82";
   };
   arm-trusted-firmware = {
-    rev = "ayufan-rock64/linux-build/0.6.14";
+    rev = "ayufan-rock64/linux-build/0.6.16";
     sha256 = "1rxp6b1mzmr3w2gg3n1r7rnk29nj92ilcramhy8119zf90by1hxv";
   };
 }
