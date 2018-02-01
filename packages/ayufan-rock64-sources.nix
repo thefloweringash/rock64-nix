4163963e7a7778cae84b90262418b9a7bcb92e7c
Andrew Childs <lorne@cons.org.nz>
2018-02-01 21:54:19 +0900

    0.6.16 -> 0.6.17

--- packages/ayufan-rock64-sources.nix
+++ packages/ayufan-rock64-sources.nix
@@ -1,19 +1,19 @@
 {
-  version = "0.6.16";
+  version = "0.6.17";
   linux-kernel = {
-    rev = "ayufan-rock64/linux-build/0.6.16";
-    sha256 = "11kbdq9rr5cx32sbbzcgxfi14mlgavbvmk1glq6vbq992n5v1ab4";
+    rev = "ayufan-rock64/linux-build/0.6.17";
+    sha256 = "11d5pjvacs3jr8jzqbqqq52989d6l8z2ymn1jg2dz6gj2wcb323k";
   };
   linux-mainline-kernel = {
-    rev = "ayufan-rock64/linux-build/0.6.16";
-    sha256 = "05a1hpfzmggkfsfdjm8la5469fkg1bb7ain5j0gcv6sp05w046cl";
+    rev = "ayufan-rock64/linux-build/0.6.17";
+    sha256 = "06ca1xdkgw9hcxzmg0z5asxq70aapx6b7ya2k6r0zlj57hvfin81";
   };
   linux-u-boot = {
-    rev = "ayufan-rock64/linux-build/0.6.16";
+    rev = "ayufan-rock64/linux-build/0.6.17";
     sha256 = "1024pamidiwaxs9qkqdbn2lv0b2ha5qh6izs8h58d1wqsis9pn82";
   };
   arm-trusted-firmware = {
-    rev = "ayufan-rock64/linux-build/0.6.16";
+    rev = "ayufan-rock64/linux-build/0.6.17";
     sha256 = "1rxp6b1mzmr3w2gg3n1r7rnk29nj92ilcramhy8119zf90by1hxv";
   };
 }
